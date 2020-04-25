2fea19884f25bc657f80c06294b0ebdb36cb221d
tracer-ics2017 <tracer@njuics.org>
2020-04-25 08:51:05 +0000

    >  compile 1410576 root Linux ef3240e6768c 4.9.184-linuxkit #1 SMP Tue Jul 2 22:58:16 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux  08:51:05 up  7:22,  0 users,  load average: 0.03, 0.09, 0.08 4a91bfb713f2b2c937e4033c5194915ac9ffb1a

--- nemu/src/monitor/debug/ui.c
+++ nemu/src/monitor/debug/ui.c
@@ -202,8 +202,8 @@ static int cmd_x(char *args)
     return 0;
   }
   int N = atoi(arg1);
-  bool success = true;
-  uint32_t target_addr = expr(arg2, &success);
+  bool *success = &(bool){true};
+  uint32_t target_addr = expr(arg2, success);
   if (success == false)
   {
     printf("Error: not a valid expr..\n");
