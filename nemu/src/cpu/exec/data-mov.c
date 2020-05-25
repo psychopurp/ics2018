e864ec66f47a6403e95ac59510ff9973426c7b2a
tracer-ics2017 <tracer@njuics.org>
2020-05-25 09:28:37 +0000

    >  compile 1410576 root Linux ef3240e6768c 4.9.184-linuxkit #1 SMP Tue Jul 2 22:58:16 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux  09:28:37 up  2:11,  0 users,  load average: 0.11, 0.10, 0.08 ea29ac9999f810ac5238c1d9bf0f573eb365f

--- nemu/src/cpu/exec/data-mov.c
+++ nemu/src/cpu/exec/data-mov.c
@@ -1,76 +1,91 @@
 #include "cpu/exec.h"
 
-make_EHelper(mov) {
+make_EHelper(mov)
+{
   operand_write(id_dest, &id_src->val);
   print_asm_template2(mov);
 }
 
-make_EHelper(push) {
+make_EHelper(push)
+{
   TODO();
-
+  rtl_push(&id_dest->val);
   print_asm_template1(push);
 }
 
-make_EHelper(pop) {
+make_EHelper(pop)
+{
   TODO();
 
   print_asm_template1(pop);
 }
 
-make_EHelper(pusha) {
+make_EHelper(pusha)
+{
   TODO();
 
   print_asm("pusha");
 }
 
-make_EHelper(popa) {
+make_EHelper(popa)
+{
   TODO();
 
   print_asm("popa");
 }
 
-make_EHelper(leave) {
+make_EHelper(leave)
+{
   TODO();
 
   print_asm("leave");
 }
 
-make_EHelper(cltd) {
-  if (decoding.is_operand_size_16) {
+make_EHelper(cltd)
+{
+  if (decoding.is_operand_size_16)
+  {
     TODO();
   }
-  else {
+  else
+  {
     TODO();
   }
 
   print_asm(decoding.is_operand_size_16 ? "cwtl" : "cltd");
 }
 
-make_EHelper(cwtl) {
-  if (decoding.is_operand_size_16) {
+make_EHelper(cwtl)
+{
+  if (decoding.is_operand_size_16)
+  {
     TODO();
   }
-  else {
+  else
+  {
     TODO();
   }
 
   print_asm(decoding.is_operand_size_16 ? "cbtw" : "cwtl");
 }
 
-make_EHelper(movsx) {
+make_EHelper(movsx)
+{
   id_dest->width = decoding.is_operand_size_16 ? 2 : 4;
   rtl_sext(&t2, &id_src->val, id_src->width);
   operand_write(id_dest, &t2);
   print_asm_template2(movsx);
 }
 
-make_EHelper(movzx) {
+make_EHelper(movzx)
+{
   id_dest->width = decoding.is_operand_size_16 ? 2 : 4;
   operand_write(id_dest, &id_src->val);
   print_asm_template2(movzx);
 }
 
-make_EHelper(lea) {
+make_EHelper(lea)
+{
   rtl_li(&t2, id_src->addr);
   operand_write(id_dest, &t2);
   print_asm_template2(lea);
