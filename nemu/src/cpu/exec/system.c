e742d2b60e2660c31f16eb7d34aedaabc6f96fb8
tracer-ics2017 <tracer@njuics.org>
2020-05-28 02:44:49 +0000

    >  compile 1410576 root Linux ef3240e6768c 4.9.184-linuxkit #1 SMP Tue Jul 2 22:58:16 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux  02:44:49 up 1 day,  4:55,  0 users,  load average: 2.00, 3.25, 2.67 5de5093ed076560f3a7aeb1d4a4e0eadca1bc9a

--- nemu/src/cpu/exec/system.c
+++ nemu/src/cpu/exec/system.c
@@ -79,9 +79,23 @@ make_EHelper(in)
 
 make_EHelper(out)
 {
-  TODO();
-  pio_write(id_dest->val, id_src->width, id_src->val);
+  // TODO();
+  operand_write(id_dest, &id_src->val);
 
+  switch (id_src->width)
+  {
+  case 4:
+    pio_write_l(id_dest->val, id_src->val);
+    break;
+  case 1:
+    pio_write_b(id_dest->val, id_src->val);
+    break;
+  case 2:
+    pio_write_w(id_dest->val, id_src->val);
+    break;
+  default:
+    assert(0);
+  }
   print_asm_template2(out);
 
 #ifdef DIFF_TEST
