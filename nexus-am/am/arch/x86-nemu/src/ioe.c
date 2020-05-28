52046981ab03b53afee5244ada7b85ebfa36fda4
tracer-ics2017 <tracer@njuics.org>
2020-05-28 04:03:14 +0000

    >  run 1410576 root Linux ef3240e6768c 4.9.184-linuxkit #1 SMP Tue Jul 2 22:58:16 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux  04:03:14 up 1 day,  6:13,  0 users,  load average: 1.10, 0.98, 0.89 d0c1378ace7b8821790bfc09362aa2771f53b28d

--- nexus-am/am/arch/x86-nemu/src/ioe.c
+++ nexus-am/am/arch/x86-nemu/src/ioe.c
@@ -26,10 +26,19 @@ extern void *memcpy(void *, const void *, int);
 
 void _draw_rect(const uint32_t *pixels, int x, int y, int w, int h)
 {
+  // int i;
+  // for (i = 0; i < _screen.width * _screen.height; i++)
+  // {
+  //   fb[i] = i;
+  // }
+
+  int temp = (w > -_screen.width - x) ? _screen.width - x : w;
+  int cp_bytes = sizeof(uint32_t) * temp;
   int i;
-  for (i = 0; i < _screen.width * _screen.height; i++)
+  for (i = 0; i < h && y + i < _screen.height; i++)
   {
-    fb[i] = i;
+    memcpy(&fb[(y + i) * _screen.width + x], pixels, cp_bytes);
+    pixels += w;
   }
 }
 
