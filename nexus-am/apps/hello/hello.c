6cf4ee525559b15067c461f21cdb81b561cca518
tracer-ics2017 <tracer@njuics.org>
2020-05-28 02:45:50 +0000

    >  run 1410576 root Linux ef3240e6768c 4.9.184-linuxkit #1 SMP Tue Jul 2 22:58:16 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux  02:45:50 up 1 day,  4:56,  0 users,  load average: 3.58, 3.51, 2.80 1a30cc9b9fe82f13726d369b17beeae984215263

--- nexus-am/apps/hello/hello.c
+++ nexus-am/apps/hello/hello.c
@@ -1,12 +1,16 @@
 #include <am.h>
 
-void print(const char *s) {
-  for (; *s; s ++) {
+void print(const char *s)
+{
+  for (; *s; s++)
+  {
     _putc(*s);
   }
 }
-int main() {
-  for (int i = 0; i < 10; i ++) {
-    print("Hello World!\n");
+int main()
+{
+  for (int i = 0; i < 10; i++)
+  {
+    // print("Hello World!\n");
   }
 }
