91cfaaceaf411d95b73abb3b64beb9a4d724882d
tracer-ics2017 <tracer@njuics.org>
2020-05-30 18:35:36 +0000

    >  run 1410576 root Linux ef3240e6768c 4.9.184-linuxkit #1 SMP Tue Jul 2 22:58:16 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux  18:35:36 up 1 day, 21:52,  0 users,  load average: 0.16, 0.12, 0.09 aea900e36b9d08d998a8fd70138276d05cfd697f

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -14,6 +14,7 @@ int sys_write(int fd, void *buf, size_t len)
   if (fd == 1 || fd == 2)
   {
     char c;
+    Log("buffer:%s", (char *)buf);
     for (int i = 0; i < len; i++)
     {
       memcpy(&c, buf + i, 1);
@@ -24,6 +25,11 @@ int sys_write(int fd, void *buf, size_t len)
   else
     panic("Unhandled fd =%d in sys_write", fd);
   return -1;
+}
+
+int sys_brk(int addr)
+{
+  return 0;
 }
 
 _RegSet *do_syscall(_RegSet *r)
@@ -43,6 +49,9 @@ _RegSet *do_syscall(_RegSet *r)
     break;
   case SYS_write:
     SYSCALL_ARG1(r) = sys_write(a[1], (void *)a[2], a[3]);
+    break;
+  case SYS_brk:
+    SYSCALL_ARG1(r) = sys_brk(a[1]);
     break;
   default:
     panic("Unhandled syscall ID = %d", a[0]);
