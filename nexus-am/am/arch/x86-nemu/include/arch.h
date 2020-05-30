09924fdc58147f51a81351154c5777f709b81927
tracer-ics2017 <tracer@njuics.org>
2020-05-30 16:26:47 +0000

    >  run 1410576 root Linux ef3240e6768c 4.9.184-linuxkit #1 SMP Tue Jul 2 22:58:16 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux  16:26:47 up 1 day, 19:43,  0 users,  load average: 0.35, 0.26, 0.21 a96b6892ed568d84dbe25ad723d95ec322ce2ca

--- nexus-am/am/arch/x86-nemu/include/arch.h
+++ nexus-am/am/arch/x86-nemu/include/arch.h
@@ -10,7 +10,7 @@ struct _RegSet
 {
   // uintptr_t esi, ebx, eax, eip, edx, error_code, eflags, ecx, cs, esp, edi, ebp;
   // int       irq;
-  uintptr_t edi, esi, ebp, esp, ebx, ecx, eax;
+  uintptr_t edi, esi, ebp, esp, ebx, edx, ecx, eax;
   int irq;
   uintptr_t error_code;
   uintptr_t eip;
@@ -19,9 +19,9 @@ struct _RegSet
 };
 
 #define SYSCALL_ARG1(r) r->eax
-#define SYSCALL_ARG2(r) 0
-#define SYSCALL_ARG3(r) 0
-#define SYSCALL_ARG4(r) 0
+#define SYSCALL_ARG2(r) r->ebx
+#define SYSCALL_ARG3(r) r->ecx
+#define SYSCALL_ARG4(r) r->edx
 
 #ifdef __cplusplus
 extern "C"
