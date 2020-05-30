98a6b8d3fefc701fb85d32ebd53444ff226baa7f
tracer-ics2017 <tracer@njuics.org>
2020-05-30 17:09:49 +0000

    >  run 1410576 root Linux ef3240e6768c 4.9.184-linuxkit #1 SMP Tue Jul 2 22:58:16 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux  17:09:49 up 1 day, 20:26,  0 users,  load average: 0.58, 0.26, 0.17 bd8c5d92d1a6c0b8a9eeb86345725e8cbf1fc194

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -4,6 +4,10 @@ int sys_none()
 {
   return 1;
 }
+void sys_exit(int a)
+{
+  _halt(a);
+}
 
 _RegSet *do_syscall(_RegSet *r)
 {
@@ -17,6 +21,9 @@ _RegSet *do_syscall(_RegSet *r)
   {
   case SYS_none:
     SYSCALL_ARG1(r) = sys_none();
+    break;
+  case SYS_exit:
+    sys_exit(a[1]);
     break;
   default:
     panic("Unhandled syscall ID = %d", a[0]);
