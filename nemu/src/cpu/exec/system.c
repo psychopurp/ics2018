b09c906e5682642e4612b805263ad2c201c582e0
tracer-ics2017 <tracer@njuics.org>
2020-05-30 16:13:12 +0000

    >  compile 1410576 root Linux ef3240e6768c 4.9.184-linuxkit #1 SMP Tue Jul 2 22:58:16 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux  16:13:12 up 1 day, 19:29,  0 users,  load average: 0.26, 0.18, 0.18 4ce1da7a63fd26dc4e3e43737ad47a358bdac9c4

--- nemu/src/cpu/exec/system.c
+++ nemu/src/cpu/exec/system.c
@@ -39,9 +39,7 @@ extern void raise_intr(uint8_t NO, vaddr_t ret_addr);
 make_EHelper(int)
 {
   // TODO();
-  uint8_t NO = id_dest->val & 0xff;
-  Log("NO %d", NO);
-  raise_intr(NO, decoding.seq_eip);
+  raise_intr(id_dest->val & 0xff, decoding.seq_eip);
 
   print_asm("int %s", id_dest->str);
 
