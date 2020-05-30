421fe11a0a506ed8c544df624cc1ba72e5ea4f0c
tracer-ics2017 <tracer@njuics.org>
2020-05-30 19:11:30 +0000

    >  run 1410576 root Linux ef3240e6768c 4.9.184-linuxkit #1 SMP Tue Jul 2 22:58:16 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux  19:11:30 up 1 day, 22:28,  0 users,  load average: 0.08, 0.08, 0.08 eb42b7164d74c89bf233dc002ce6b3c933fa4369

--- nanos-lite/include/fs.h
+++ nanos-lite/include/fs.h
@@ -3,6 +3,16 @@
 
 #include "common.h"
 
-enum {SEEK_SET, SEEK_CUR, SEEK_END};
+enum
+{
+    SEEK_SET,
+    SEEK_CUR,
+    SEEK_END
+};
+
+int fs_open(const char *filename, int flags, int mode);
+ssize_t fs_read(int fd, void *buf, size_t len);
+int fs_close(int fd);
+size_t fs_filesz(int fd);
 
 #endif
--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -1,26 +1,105 @@
 #include "fs.h"
 
-typedef struct {
+extern void ramdisk_read(void *buf, off_t offset, size_t len);
+extern void ramdisk_write(void *buf, off_t offset, size_t len);
+
+typedef struct
+{
   char *name;
   size_t size;
   off_t disk_offset;
+  off_t open_offset;
 } Finfo;
 
-enum {FD_STDIN, FD_STDOUT, FD_STDERR, FD_FB, FD_EVENTS, FD_DISPINFO, FD_NORMAL};
+enum
+{
+  FD_STDIN,
+  FD_STDOUT,
+  FD_STDERR,
+  FD_FB,
+  FD_EVENTS,
+  FD_DISPINFO,
+  FD_NORMAL
+};
 
 /* This is the information about all files in disk. */
 static Finfo file_table[] __attribute__((used)) = {
-  {"stdin (note that this is not the actual stdin)", 0, 0},
-  {"stdout (note that this is not the actual stdout)", 0, 0},
-  {"stderr (note that this is not the actual stderr)", 0, 0},
-  [FD_FB] = {"/dev/fb", 0, 0},
-  [FD_EVENTS] = {"/dev/events", 0, 0},
-  [FD_DISPINFO] = {"/proc/dispinfo", 128, 0},
+    {"stdin (note that this is not the actual stdin)", 0, 0},
+    {"stdout (note that this is not the actual stdout)", 0, 0},
+    {"stderr (note that this is not the actual stderr)", 0, 0},
+    [FD_FB] = {"/dev/fb", 0, 0},
+    [FD_EVENTS] = {"/dev/events", 0, 0},
+    [FD_DISPINFO] = {"/proc/dispinfo", 128, 0},
 #include "files.h"
 };
 
 #define NR_FILES (sizeof(file_table) / sizeof(file_table[0]))
 
-void init_fs() {
+void init_fs()
+{
   // TODO: initialize the size of /dev/fb
+}
+
+//获取文件大小
+size_t fs_filesz(int fd)
+{
+  assert(fd >= 0 && fd < NR_FILES);
+  return file_table[fd].size;
+}
+
+off_t disk_offset(int fd)
+{
+  assert(fd >= 0 && fd < NR_FILES);
+  return file_table[fd].disk_offset;
+}
+
+off_t get_open_offset(int fd)
+{
+  assert(fd >= 0 && fd < NR_FILES);
+  return file_table[fd].open_offset;
+}
+
+void set_open_offset(int fd, off_t n)
+{
+  assert(fd >= 0 && fd < NR_FILES);
+  assert(n >= 0);
+  if (n > file_table[fd].size)
+  {
+    n = file_table[fd].size;
+  }
+  file_table[fd].open_offset = n;
+}
+
+int fs_open(const char *filename, int flags, int mode)
+{
+  //根据文件名查找索引
+  for (int i = 0; i < NR_FILES; i++)
+  {
+    if (strcmp(filename, file_table[i].name) == 0)
+      return i;
+  }
+  panic("this filename not exist");
+  return -1;
+}
+
+ssize_t fs_read(int fd, void *buf, size_t len)
+{
+  assert(fd >= 0 && fd < NR_FILES);
+  if (fd < 3)
+  {
+    Log("arg invalid:fd<3");
+    return 0;
+  }
+  int n = fs_filesz(fd) - get_open_offset(fd);
+  if (n > len)
+    n = len;
+  ramdisk_read(buf, disk_offset(fd) + get_open_offset(fd), n);
+  set_open_offset(fd, get_open_offset(fd) + n);
+  return n;
+}
+
+int fs_close(int fd)
+{
+  assert(fd >= 0 && fd < NR_FILES);
+  return 0;
 }
