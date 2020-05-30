02ec86fe68254a2efd79c5170e3550d492005521
tracer-ics2017 <tracer@njuics.org>
2020-05-30 04:06:14 +0000

    >  compile 1410576 root Linux ef3240e6768c 4.9.184-linuxkit #1 SMP Tue Jul 2 22:58:16 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux  04:06:14 up 1 day, 15:39,  0 users,  load average: 0.48, 0.28, 0.18 deaf45e677ddc5c2c1a771ce5a09157a2dd0fe

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -2,7 +2,14 @@
 
 #define DEFAULT_ENTRY ((void *)0x4000000)
 
-uintptr_t loader(_Protect *as, const char *filename) {
-  TODO();
+extern uint8_t ramdisk_start;
+extern uint8_t ramdisk_end;
+#define RAMDISK_SIZE ((&ramdisk_end) - (&ramdisk_start))
+extern void ramdisk_read(void *buf, off_t offset, size_t len);
+
+uintptr_t loader(_Protect *as, const char *filename)
+{
+  // TODO();
+  ramdisk_read(DEFAULT_ENTRY, 0, RAMDISK_SIZE);
   return (uintptr_t)DEFAULT_ENTRY;
 }
