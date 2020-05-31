c035264d4d558f569c80e46478cbb3976c0f413f
tracer-ics2017 <tracer@njuics.org>
2020-05-31 06:22:52 +0000

    >  run 1410576 root Linux ef3240e6768c 4.9.184-linuxkit #1 SMP Tue Jul 2 22:58:16 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux  06:22:52 up 2 days,  1:45,  0 users,  load average: 0.16, 0.24, 0.20 6bb9c9be28d4bc09f685214c40a4485ebb1f6c5

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -95,6 +95,7 @@ int fs_open(const char *filename, int flags, int mode)
 // void fb_write(const void *buf, off_t offset, size_t len);
 ssize_t fs_read(int fd, void *buf, size_t len)
 {
+  Log("read-------");
   assert(fd >= 0 && fd < NR_FILES);
   if (fd < 3 || fd == FD_FB)
   {
