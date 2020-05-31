b23568ab11d51d785d695a11eeb24aa3b3073991
tracer-ics2017 <tracer@njuics.org>
2020-05-31 06:26:37 +0000

    >  run 1410576 root Linux ef3240e6768c 4.9.184-linuxkit #1 SMP Tue Jul 2 22:58:16 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux  06:26:37 up 2 days,  1:49,  0 users,  load average: 0.21, 0.22, 0.19 4e8f30d74c406bebeb5136bf9869c91ac885b8d

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -95,7 +95,6 @@ int fs_open(const char *filename, int flags, int mode)
 // void fb_write(const void *buf, off_t offset, size_t len);
 ssize_t fs_read(int fd, void *buf, size_t len)
 {
-  Log("read-------");
   assert(fd >= 0 && fd < NR_FILES);
   if (fd < 3 || fd == FD_FB)
   {
@@ -123,9 +122,9 @@ ssize_t fs_write(int fd, void *buf, size_t len)
 {
 
   assert(fd >= 0 && fd < NR_FILES);
-  if (fd < 3)
+  if (fd < 3 || fd == FD_DISPINFO)
   {
-    Log("arg invalid:fd<3");
+    Log("arg invalid:fd<3 || fd==FD_DISPINFO");
     return 0;
   }
   //最多写入字节数
