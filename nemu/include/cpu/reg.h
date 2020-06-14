3bb44b5f3d6cf78ce9ebf2768d8c193a58caa1e4
tracer-ics2017 <tracer@njuics.org>
2020-06-14 05:51:23 +0000

    >  compile 1410576 root Linux ef3240e6768c 4.9.184-linuxkit #1 SMP Tue Jul 2 22:58:16 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux  05:51:23 up 13:06,  0 users,  load average: 0.34, 0.33, 0.39 b988a9085232c92db78aa650219538768264b2b7

--- nemu/include/cpu/reg.h
+++ nemu/include/cpu/reg.h
@@ -63,35 +63,36 @@ typedef struct
     {
       rtlreg_t eax, ecx, edx, ebx, esp, ebp, esi, edi;
       vaddr_t eip;
-    };
-    //实现eflags寄存器
-    union {
-      struct
-      {
-        unsigned int CF : 1;
-        unsigned int one : 1; //理论上保持为1，可以用来验证下初始化
-        unsigned int : 4;
-        unsigned int ZF : 1;
-        unsigned int SF : 1; //bin 0-7
 
-        unsigned int : 1;
-        unsigned int IF : 1;
-        unsigned int : 1;
-        unsigned int OF : 1;
-        unsigned int : 20; //bin 8-31
+      //实现eflags寄存器
+      union {
+        struct
+        {
+          unsigned int CF : 1;
+          unsigned int one : 1; //理论上保持为1，可以用来验证下初始化
+          unsigned int : 4;
+          unsigned int ZF : 1;
+          unsigned int SF : 1; //bin 0-7
+
+          unsigned int : 1;
+          unsigned int IF : 1;
+          unsigned int : 1;
+          unsigned int OF : 1;
+          unsigned int : 20; //bin 8-31
+        };
+        unsigned int eflags;
       };
-      unsigned int eflags;
+
+      rtlreg_t cs;
+
+      //实现IDTR寄存器
+      struct IDTR
+      {
+        uint32_t base;
+        uint16_t limit; //IDT的首地址和长度
+
+      } idtr;
     };
-
-    rtlreg_t cs;
-
-    //实现IDTR寄存器
-    struct IDTR
-    {
-      uint32_t base;
-      uint16_t limit; //IDT的首地址和长度
-
-    } idtr;
   };
   CR3 cr3;
   CR0 cr0;
